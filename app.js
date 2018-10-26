7d52793362b40b85ab06176aedca5534b1943d76
mikezalik <mike.zalik@gmail.com>
2018-10-26 10:35:31 -0700

    JS update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 
 // Variables
-var api = "http://api.openweathermap.org/data/2.5/weather?";
+var api = "https://api.openweathermap.org/data/2.5/weather?";
 var key = "&APPID=8c77ba2f06ce2e1985605723650676a9";
 var lat, lon;
 
