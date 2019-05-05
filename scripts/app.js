9fdb8d39d0eacbae1175917b03e91d7ddadffb89
mikezalik <mike.zalik@gmail.com>
2019-05-05 12:06:24 -0700

    unix time conversion

--- scripts/app.js
+++ scripts/app.js
@@ -39,11 +39,27 @@ function getWeather(lat, lon) {
             var myArr= JSON.parse(this.responseText);
 
             tempConv = Math.round((myArr.main.temp - 273.15) * 1.8 + 32) + String.fromCharCode (176);
+
+            function unixTimeConversion (myArr) {
+                
+
+                var months_arr = ['Jan','Feb','Mar','Apr','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+                var date = new Date(myArr.list.dt * 1000);
+                var year = date.getFullYear();
+                var month = months_arr[date.getMonth()];
+                var day = date.getDate();
+                
+                var convTime = month+'-'+day+'-'+year;
+                document.getElementById('datetime').innerHTML = convTime;
+
+
+            }
             
             document.getElementById("city").innerHTML = myArr.name;
             document.getElementById("temperature").innerHTML = tempConv;
             document.getElementById("humidity").innerHTML = myArr.main.humidity + "%";
             document.getElementById("summary").innerHTML = myArr.weather[0].description;
+            unixTimeConversion(myArr);
         };
 }
 
