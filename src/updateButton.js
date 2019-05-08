f7f2c01e006c1ff572617ee03278cf7b86bb56dd
mikezalik <mike.zalik@gmail.com>
2019-05-07 21:38:47 -0700

    JSX

---
+++ src/updateButton.js
@@ -0,0 +1,23 @@
+'use strict';
+
+class UpdateButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { update: false };
+  }
+
+  render() {
+    if (this.state.updated) {
+      return 'Your location is updated';
+    }
+
+    return (
+      <button onClick={() => this.setState({ update: true }) }>
+        Update
+      </button>
+    );
+  }
+}
+
+let domContainer = document.querySelector('#update_button_container');
+ReactDOM.render(<UpdateButton />, domContainer);
