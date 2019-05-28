327625f59b76fd614af4b2c932f376324a93a552
mikezalik <mike.zalik@gmail.com>
2019-05-27 22:08:17 -0700

    index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.css';
-import WeatherInfo from '.weatherInfo.js';
+import WeatherInfo from './weatherInfo.js';
 import registerServiceWorker from './registerServiceWorker';
-ReactDOM.render(<App />, document.getElementById(''));
+ReactDOM.render(<WeatherInfo />, document.getElementById(''));
 registerServiceWorker();
