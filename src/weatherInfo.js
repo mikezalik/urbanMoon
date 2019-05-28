ee6a55ec3bbc5505dfc6f8d92defcfd2f41ef5f7
mikezalik <mike.zalik@gmail.com>
2019-05-27 21:44:28 -0700

    weatherInfo

--- src/weatherInfo.js
+++ src/weatherInfo.js
@@ -4,19 +4,10 @@ import { myArr } from '../app.js';
 
 export class WeatherInfo extends React.Component {
     render () {
-        const date = document.getElementById("currentDateTime").innerHTML = myArr.list[0].dt_txt;
-        const date2 = document.getElementById("day2DateTime").innerHTML = myArr.list[8].dt_txt;
-        const date3 = document.getElementById("day3DateTime").innerHTML = myArr.list[16].dt_txt;
-        const date4 = document.getElementById("day4DateTime").innerHTML = myArr.list[24].dt_txt;
-        const date5 = document.getElementById("day5DateTime").innerHTML = myArr.list[32].dt_txt;   
         return (
             <div>
-                <div>
-                    <h2>{date}</h2>
-                </div>
-                <div>
-                    <h2>{date2}</h2>
-                </div>
+
+            </div>
                 <div>
                     <h2>{date3}</h2>
                 </div>
