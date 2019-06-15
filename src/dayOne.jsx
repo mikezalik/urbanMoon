98ab511fe8c9f0939b7003b5dfe48d4e0422564e
mikezalik <mike.zalik@gmail.com>
2019-06-15 15:28:35 -0700

    jsx refactor

---
+++ src/dayOne.jsx
@@ -0,0 +1,26 @@
+class DayOne extends React.Component {
+  render() {
+    return (
+      <div>
+        <div className="col">
+              <h2>Date</h2>
+              <span id="currentDateTime" />
+            </div>
+        <div className="col">
+              <h2>Temperature</h2>
+              <span id="currentTemperature" />
+            </div>
+        <div className="col">
+              <h2>Humidity</h2>
+              <span id="currentHumidity" />
+            </div>
+        <div className="col">
+              <h2>Summary</h2>
+              <span id="currentSummary" />
+            </div>
+      </div>
+    );
+  }
+}
+
+export default DayOne;
