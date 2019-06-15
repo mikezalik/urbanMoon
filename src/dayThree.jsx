e1317256312c2577da3e92d29c56f21303f6e3b0
mikezalik <mike.zalik@gmail.com>
2019-06-15 15:33:34 -0700

    jsx refactor

---
+++ src/dayThree.jsx
@@ -0,0 +1,12 @@
+import { WeatherInfo } from './weatherInfo';
+
+class DayThree extends React.Component {
+  render() {
+      <div>
+          <WeatherInfo />
+
+        </div>;
+  }
+}
+
+ReactDOM.render(<DayThree />, document.querySelector('#row align-items-center'));
