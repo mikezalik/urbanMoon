abd73abc6f5da95bc2e026feb71b18b09000185f
mikezalik <mike.zalik@gmail.com>
2019-06-15 15:17:36 -0700

    refactor

--- app.js
+++ app.js
@@ -3,11 +3,6 @@
 // Variables
 const api = 'https://api.openweathermap.org/data/2.5/forecast?';
 const key = '&APPID=8c77ba2f06ce2e1985605723650676a9';
-let lat;
-let lon;
-
-// geoLocate on page load
-document.addEventListener('load', locate());
 
 // Geolocation - lat, lon for openweather API
 const locate = function geoLocate() {
@@ -21,51 +16,55 @@ const locate = function geoLocate() {
 
 // lat, lon to call getWeather and retrieve JSON
 const success = function geoLocateSuccess(position) {
-  const lat = 'lat=' + position.coords.latitude;
-  const lon = 'lon=' + position.coords.longitude;
+  const lat = `lat=${position.coords.latitude}`;
+  const lon = `lon=${position.coords.longitude}`;
   getWeather(lat, lon);
 };
+
+// geoLocate on page load
+document.addEventListener('load', locate());
+
 
 // AJAX request
 function getWeather(lat, lon) {
   const httpRequest = new XMLHttpRequest();
-  const urlString = `${api + lat  }&${  lon  }${key}`;
+  const urlString = `${api + lat}&${lon}${key}`;
 
   httpRequest.open('GET', urlString);
   httpRequest.send();
 
-  httpRequest.onload = function () {
-    var myArr = httpRequest.response;
-    var myArr = JSON.parse(this.responseText);
+  httpRequest.onload = function httpR() {
+    const myArrHttp = httpRequest.response;
+    let myArr = JSON.parse(this.responseText);
 
     // Temperature conversion
     tempConv = Math.round((myArr.list[0].main.temp - 273.15) * 1.8 + 32) + String.fromCharCode(176);
 
     // Populate DOM
-    document.getElementById('city').innerHTML = 'Location \n' + myArr.city.name;
+    document.getElementById('city').innerHTML = `Location \n${myArr.city.name}`;
     document.getElementById('currentTemperature').innerHTML = tempConv;
-    document.getElementById('currentHumidity').innerHTML = `${myArr.list[0].main.humidity  }%`;
+    document.getElementById('currentHumidity').innerHTML = `${myArr.list[0].main.humidity}%`;
     document.getElementById('currentSummary').innerHTML = myArr.list[0].weather[0].description;
     document.getElementById('currentDateTime').innerHTML = myArr.list[0].dt_txt;
     // Day 2
-    document.getElementById('day2Temperature').innerHTML = tempConv;
-    document.getElementById('day2Humidity').innerHTML = `${myArr.list[8].main.humidity  }%`;
+    /* document.getElementById('day2Temperature').innerHTML = tempConv;
+    document.getElementById('day2Humidity').innerHTML = `${myArr.list[8].main.humidity}%`;
     document.getElementById('day2Summary').innerHTML = myArr.list[8].weather[0].description;
     document.getElementById('day2DateTime').innerHTML = myArr.list[8].dt_txt;
     // Day 3
     document.getElementById('day3Temperature').innerHTML = tempConv;
-    document.getElementById('day3Humidity').innerHTML = `${myArr.list[16].main.humidity  }%`;
+    document.getElementById('day3Humidity').innerHTML = `${myArr.list[16].main.humidity}%`;
     document.getElementById('day3Summary').innerHTML = myArr.list[16].weather[0].description;
     document.getElementById('day3DateTime').innerHTML = myArr.list[16].dt_txt;
     // Day 4
     document.getElementById('day4Temperature').innerHTML = tempConv;
-    document.getElementById('day4Humidity').innerHTML = `${myArr.list[24].main.humidity  }%`;
+    document.getElementById('day4Humidity').innerHTML = `${myArr.list[24].main.humidity}%`;
     document.getElementById('day4Summary').innerHTML = myArr.list[24].weather[0].description;
     document.getElementById('day4DateTime').innerHTML = myArr.list[24].dt_txt;
     // Day 5
     document.getElementById('day5Temperature').innerHTML = tempConv;
-    document.getElementById('day5Humidity').innerHTML = `${myArr.list[32].main.humidity  }%`;
+    document.getElementById('day5Humidity').innerHTML = `${myArr.list[32].main.humidity}%`;
     document.getElementById('day5Summary').innerHTML = myArr.list[32].weather[0].description;
-    document.getElementById('day5DateTime').innerHTML = myArr.list[32].dt_txt;
+    document.getElementById('day5DateTime').innerHTML = myArr.list[32].dt_txt; */
   };
 }
