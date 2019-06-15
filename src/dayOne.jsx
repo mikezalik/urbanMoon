f4e93a4ba703a751d52ac05dd11106cf1e3206fe
mikezalik <mike.zalik@gmail.com>
2019-06-15 15:31:04 -0700

    jsx refactor

--- src/dayOne.jsx
+++ src/dayOne.jsx
@@ -3,21 +3,21 @@ class DayOne extends React.Component {
     return (
       <div>
         <div className="col">
-              <h2>Date</h2>
-              <span id="currentDateTime" />
-            </div>
+          <h2>Date</h2>
+          <span id="currentDateTime" />
+        </div>
         <div className="col">
-              <h2>Temperature</h2>
-              <span id="currentTemperature" />
-            </div>
+          <h2>Temperature</h2>
+          <span id="currentTemperature" />
+        </div>
         <div className="col">
-              <h2>Humidity</h2>
-              <span id="currentHumidity" />
-            </div>
+          <h2>Humidity</h2>
+          <span id="currentHumidity" />
+        </div>
         <div className="col">
-              <h2>Summary</h2>
-              <span id="currentSummary" />
-            </div>
+          <h2>Summary</h2>
+          <span id="currentSummary" />
+        </div>
       </div>
     );
   }
