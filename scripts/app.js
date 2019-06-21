220bea9fb7f779b7f5dce5164b429282f497fe98
mikezalik <mike.zalik@gmail.com>
2019-06-21 08:33:13 -0700

    refactor

--- scripts/app.js
+++ scripts/app.js
@@ -2,10 +2,10 @@
 // TODO: React?
 
 // Variables
-let api = 'https://api.openweathermap.org/data/2.5/weather?';
-let key = '&APPID=8c77ba2f06ce2e1985605723650676a9';
-let lat; var 
-lon;
+const api = 'https://api.openweathermap.org/data/2.5/weather?';
+const key = '&APPID=8c77ba2f06ce2e1985605723650676a9';
+let lat; let
+  lon;
 
 // geoLocate on page load
 document.addEventListener('load', geoLocate());
@@ -20,16 +20,16 @@ function geoLocate() {
   }
 }
 function success(position) {
-  let lat = 'lat=' + position.coords.latitude;
-  let lon = 'lon=' + position.coords.longitude;
+  const lat = `lat=${position.coords.latitude}`;
+  const lon = `lon=${position.coords.longitude}`;
   getWeather(lat, lon);
 }
 
 
 // AJAX request
 function getWeather(lat, lon) {
-  let httpRequest = new XMLHttpRequest();
-  let urlString = `${api + lat  }&${  lon  }${key}`;
+  const httpRequest = new XMLHttpRequest();
+  const urlString = `${api + lat}&${lon}${key}`;
 
   httpRequest.open('GET', urlString);
   httpRequest.send();
@@ -42,7 +42,7 @@ function getWeather(lat, lon) {
 
     document.getElementById('city').innerHTML = myArr.name;
     document.getElementById('temperature').innerHTML = tempConv;
-    document.getElementById('humidity').innerHTML = `${myArr.main.humidity  }%`;
+    document.getElementById('humidity').innerHTML = `${myArr.main.humidity}%`;
     document.getElementById('summary').innerHTML = myArr.weather[0].description;
   };
 }
