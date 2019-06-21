5727f3076183bec8cbb635c241d784e76e1346c0
mikezalik <mike.zalik@gmail.com>
2019-06-21 08:32:53 -0700

    refactor

--- scripts/app.js
+++ scripts/app.js
@@ -2,48 +2,47 @@
 // TODO: React?
 
 // Variables
-var api = "https://api.openweathermap.org/data/2.5/weather?";
-var key = "&APPID=8c77ba2f06ce2e1985605723650676a9";
-var lat, lon;
+let api = 'https://api.openweathermap.org/data/2.5/weather?';
+let key = '&APPID=8c77ba2f06ce2e1985605723650676a9';
+let lat; var 
+lon;
 
-//geoLocate on page load
-document.addEventListener("load", geoLocate());
+// geoLocate on page load
+document.addEventListener('load', geoLocate());
 
 // Geolocation - lat, lon for openweather API
 function geoLocate() {
-
-    if (navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(success);
-        console.log("Success");
-    } else {
-        alert("Geolocation is not supported by this browser");
-    }
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success);
+    console.log('Success');
+  } else {
+    alert('Geolocation is not supported by this browser');
+  }
 }
-    function success(position) {
-        var lat = "lat=" + position.coords.latitude;
-        var lon = "lon=" + position.coords.longitude;
-        getWeather(lat, lon);
-    }
+function success(position) {
+  let lat = 'lat=' + position.coords.latitude;
+  let lon = 'lon=' + position.coords.longitude;
+  getWeather(lat, lon);
+}
 
-    
-//AJAX request
+
+// AJAX request
 function getWeather(lat, lon) {
-    var httpRequest = new XMLHttpRequest();
-    var urlString = api + lat + "&" + lon + key;
+  let httpRequest = new XMLHttpRequest();
+  let urlString = `${api + lat  }&${  lon  }${key}`;
 
-    httpRequest.open('GET', urlString);
-    httpRequest.send();
-    
-        httpRequest.onload=function(){
-            var myArr = httpRequest.response;
-            var myArr= JSON.parse(this.responseText);
+  httpRequest.open('GET', urlString);
+  httpRequest.send();
 
-            tempConv = Math.round((myArr.main.temp - 273.15) * 1.8 + 32) + String.fromCharCode (176);
-            
-            document.getElementById("city").innerHTML = myArr.name;
-            document.getElementById("temperature").innerHTML = tempConv;
-            document.getElementById("humidity").innerHTML = myArr.main.humidity + "%";
-            document.getElementById("summary").innerHTML = myArr.weather[0].description;
-        };
+  httpRequest.onload = function () {
+    var myArr = httpRequest.response;
+    var myArr = JSON.parse(this.responseText);
+
+    tempConv = Math.round((myArr.main.temp - 273.15) * 1.8 + 32) + String.fromCharCode(176);
+
+    document.getElementById('city').innerHTML = myArr.name;
+    document.getElementById('temperature').innerHTML = tempConv;
+    document.getElementById('humidity').innerHTML = `${myArr.main.humidity  }%`;
+    document.getElementById('summary').innerHTML = myArr.weather[0].description;
+  };
 }
-
