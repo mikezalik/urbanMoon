412882ec7c7e2a638d5595e854ec2b1392316b3f
mikezalik <mike.zalik@gmail.com>
2021-01-11 13:06:43 -0800

    Adds navbar bottom and jumbotron components

---
+++ src/components/Jumbotron.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Jumbotron = () => {
+  return (
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">
+        <h1 class="display-3"></h1>
+        <h1 class="display-1">&#x1F318;</h1>
+        <p class="lead"></p>
+      </div>
+    </div>
+  );
+};
+
+export default Jumbotron;
---
+++ src/components/NavbarBottom.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NavbarBottom = () => {
+  return (
+    <div>
+      <footer>
+        <div id="navbar-b">Michael Zalik</div>
+      </footer>
+    </div>
+  );
+};
+
+export default NavbarBottom;
